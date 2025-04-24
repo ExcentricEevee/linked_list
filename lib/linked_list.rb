19c65d798f51afaa0ce6ba2e628a4882fef4f1bf
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-04-24 03:22:49 -0400

    Create initial implementation of LinkedList
    A LinkedList initializes with a Head and Tail reference (default to nil)
    and as the list populates, Head will point to the first node in the list
    and Tail to the last one.
    
    The method append was implemented in addition to this in order to give
    an initial means of populating the list by means of adding a node to the
    end of the list.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,2 +1,18 @@
+# Data structure that functions as an array but in a different way.
+# List of nodes that contain data and references each other
+# Pros: insertion/deletion is quick. Cons: lookup is slower
 class LinkedList
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+    puts "LinkedList Initialized"
+  end
+
+  def append(data)
+    node = Node.new(data)
+    head.nil? ? self.head = node : tail.next_node = node
+    self.tail = node
+  end
 end
---
+++ main.rb
@@ -0,0 +1,11 @@
+require_relative "lib/linked_list"
+require_relative "lib/node"
+
+list = LinkedList.new
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+p list

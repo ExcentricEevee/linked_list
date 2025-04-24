e710f3df5940c6af934b0ae25a7a22066a9feb82
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-04-24 02:17:48 -0400

    Setup Node class for linked list

--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,9 @@
+# Pieces of data that also reference sequential nodes. Used in linked lists.
 class Node
+  attr_accessor :value, :next_node
+
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
 end
